8b1bae3803c2f94530c22b097d297539dcf46528
hlowso <h.canderleigh@gmail.com>
2018-03-29 14:58:02 +0000

    admin/categories up

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -8,7 +8,8 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-  	@category = Category.new(params[:name])
+  	puts "The param: #{params}"
+  	@category = Category.new(category_params)
 
   	if @category.save
   		redirect_to [:admin, :categories], notice: 'Category created!'
@@ -16,5 +17,12 @@ class Admin::CategoriesController < ApplicationController
   		render :new
   	end
   end
-  
+
+  private
+  	def category_params
+  		params.require(:category).permit(
+  			:name
+  		)
+  	end
+
 end
