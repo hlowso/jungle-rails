151c7d121848adca16f03b50f4424137720cb3b0
hlowso <h.canderleigh@gmail.com>
2018-03-30 15:15:02 +0000

    admin auth done; branching to get into the dirty work of reviews & ratings

---
+++ app/models/review.rb
@@ -0,0 +1,13 @@
+class Review < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :product
+
+  validates :user_id, presence: true
+  validates :product_id, presence: true
+  validates :rating, presence: true, numericality: {
+  	greater_than_or_equal_to: 0,
+  	less_than_or_equal_to: 5
+  }
+
+
+end
