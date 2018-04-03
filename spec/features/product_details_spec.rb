87d7e4704a29e2a0207628f94eece3678bd45741
hlowso <h.canderleigh@gmail.com>
2018-04-03 18:15:16 +0000

    product details feature test functional

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+	# SETUP
+	before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can click on a product to navigate to its details page" do
+  	visit root_path
+  	product_links = page.all("a[href^='/products']")
+  	chosen_link = product_links.sample()
+  	chosen_link.trigger('click')
+  	expect(page).to have_selector('section.products-show')
+
+  end
+end
